b074b0e0fe4bdd15783342af05376571a6867b28
Pat Marion <pat.marion@kitware.com>
2013-05-06 12:39:37 +1000

    Add _dotblas to list of numpy builtin modules

--- files/helloNumpy/numpy_builtin.h
+++ files/helloNumpy/numpy_builtin.h
@@ -11,6 +11,7 @@ extern void initscalarmath();
 extern void initumath();
 extern void initumath_tests();
 extern void init_sort();
+extern void init_dotblas();
 extern void initfftpack_lite();
 extern void init_compiled_base();
 extern void initlapack_lite();
@@ -24,6 +25,7 @@ void add_numpy_builtin()
   PyImport_AppendInittab("numpy.core.scalarmath", initscalarmath);
   PyImport_AppendInittab("numpy.core.umath", initumath);
   PyImport_AppendInittab("numpy.core.umath_tests", initumath_tests);
+  PyImport_AppendInittab("numpy.core._dotblas", init_dotblas);
   PyImport_AppendInittab("numpy.core._sort", init_sort);
   PyImport_AppendInittab("numpy.fft.fftpack_lite", initfftpack_lite);
   PyImport_AppendInittab("numpy.lib._compiled_base", init_compiled_base);
